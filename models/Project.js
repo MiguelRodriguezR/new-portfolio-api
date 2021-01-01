5d6d19e60923a6829b9a9828342b6cbebbb80a39
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-01 18:33:37 -0500

    added links

--- models/Project.js
+++ models/Project.js
@@ -4,7 +4,9 @@ const ProjectSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   languages: { type: String, required: true },
-  image: { type: String, required: true }  
+  image: { type: String, required: true },
+  plink: { type: String, required: true },
+  ghlink: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Project", ProjectSchema);
