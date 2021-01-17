126563c68277ecb963afdbc79cd7aeaba373b384
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2021-01-16 23:31:08 -0500

    order changes

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,7 +3,7 @@ const Project  = require('../models/Project')
 exports.getProjects = async (req, res) =>{
     try {
         const projects = await Project.find();
-        res.json({ projects });
+        res.json({ projects: projects.reverse() });
     } catch (error) {
         console.log(error);
         res.status(500).send('internal error')
